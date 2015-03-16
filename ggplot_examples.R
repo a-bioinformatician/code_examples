a804d56d93cf9bb20a637b3d38fe38766a2ad3c2
Sam Hunter <shunter@gmail.com>
2015-03-16 11:48:40 -0400

    ggplot examples

---
+++ ggplot_examples.R
@@ -0,0 +1,9 @@
+# Default colors from GGPlot:
+# http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
+
+
+
